efc2ba3267a21dbc7cc26db6553f9edfac546e4f
tpass-bit <narayanmawale83@gmail.com>
2025-06-20 17:10:47 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,12 +5,13 @@ import { getStorage, ref as storageRef, getDownloadURL } from "https://www.gstat
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBPF1VE82Y3VkZe6IibjqKxBC-XHjM_Wco",
+  authDomain: "chat-2024-ff149.firebaseapp.com",                    // default auth domain
+  databaseURL: "https://chat-2024-ff149-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-2024-ff149",
+  storageBucket: "chat-2024-ff149.appspot.com",
+  messagingSenderId: "146349109253",
+  appId: "1:146349109253:android:e593afbf0584762519ac6c"
 };
 
 // Initialize Firebase
@@ -18,4 +19,5 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const storage = getStorage(app);
 
+// Export helpers for the rest of your modules
 export { db, storage, ref, get, child, storageRef, getDownloadURL };
